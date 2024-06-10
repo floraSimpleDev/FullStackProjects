423176313585e55458521f487e95959d4823c73b
Flora Li <samantha20200604@gmail.com>
2024-06-10 17:32:48 +1000

    initialize test code for localhost: 7000/api/test

--- mern-booking-app/backend/src/index.ts
+++ mern-booking-app/backend/src/index.ts
@@ -1,6 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 
+//create an express app, make sure each express API be parsed into JSON type
 const app = express();
-app.use(express.json);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //qs library
+app.use(cors()); //cross origin resource sharing
+
+//test code to make sure the express app API is ok.
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "hello from express endpoint!" });
+});
+
+app.listen(7000, () => {
+  console.log("server running on localhost: 7000/api/test");
+});
