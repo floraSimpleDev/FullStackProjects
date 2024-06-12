3c7a1651e125d616cd67d22e4b4d3092a662e7e3
Flora Li <samantha20200604@gmail.com>
2024-06-12 22:49:21 +1000

    set up Header.tsx

--- mern-booking-app/frontend/src/components/Header.tsx
+++ mern-booking-app/frontend/src/components/Header.tsx
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom";
+import { FaUniversalAccess } from "react-icons/fa";
+import { RxEnter } from "react-icons/rx";
 
 const Header = () => {
   return (
-    <div className="bg-blue-500 py-6">
-      <div className="container mx-auto flex justify-between">
-        <span className="text-3xl text-white font-bold tracking-tighter">
-          <Link to="/"></Link>
-        </span>
-      </div>
-    </div>
+    <nav className="bg-[#493e99] py-6">
+      <menu className="container mx-auto flex justify-between px-40">
+        <li className="flex items-center gap-3 text-3xl text-[#f09d7c] font-bold -tracking-tighter cursor-pointer">
+          <FaUniversalAccess />
+          <Link to="/">Azure Heaven</Link>
+        </li>
+        <li className="flex space-x-2">
+          <Link
+            to="/sign-in"
+            className="flex items-center text-[#f09d7c] px-3 font-bold border border-[#f09d7c] rounded-xl hover:bg-[#f09d7c] hover:text-[#493e99] transition"
+          >
+            <RxEnter />
+            &nbsp; Sign In
+          </Link>
+        </li>
+      </menu>
+    </nav>
   );
 };
 
