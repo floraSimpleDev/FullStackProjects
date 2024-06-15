6ddab199169e14bb15b900d60af4f07a8e94aac7
Flora Li <samantha20200604@gmail.com>
2024-06-16 00:36:33 +1000

    use QueryClient to manage async

--- mern-booking-app/frontend/src/main.tsx
+++ mern-booking-app/frontend/src/main.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+//managing and caching asynchronous data, simplifies the process of fetching, caching, synchronizing, and updating server-side data
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
