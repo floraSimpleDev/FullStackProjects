f6bb82e61f75a1c8d257e531bceccad8740b3857
Flora Li <samantha20200604@gmail.com>
2024-06-16 13:43:35 +1000

    set up useState to update the toastMessage

--- mern-booking-app/frontend/src/contexts/AppContext.tsx
+++ mern-booking-app/frontend/src/contexts/AppContext.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Toast from "../components/Toast";
 
 type ToastMessage = {
   message: string;
@@ -17,15 +18,24 @@ export const AppContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const [toast, setToast] = useState<ToastMessage | undefined>(undefined);
   return (
     <AppContext.Provider
       value={{
         showToast: (toastMessage) => {
           //declare toastMessage in (params)
-          console.log(toastMessage);
+          setToast(toastMessage);
+          /* console.log(toastMessage); */
         },
       }}
     >
+      {toast && (
+        <Toast
+          message={toast.message}
+          type={toast.type}
+          onClose={() => setToast(undefined)}
+        />
+      )}
       {children}
     </AppContext.Provider>
   );
