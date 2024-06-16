ca452631880eb096422a5e2517a7b796300ae283
Flora Li <samantha20200604@gmail.com>
2024-06-16 13:25:23 +1000

    set up Toast.tsx displaying layout

---
+++ mern-booking-app/frontend/src/components/Toast.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+
+type ToastProps = {
+  message: string;
+  type: "SUCCESS" | "ERROR";
+  onClose: () => void;
+};
+
+const Toast = ({ message, type, onClose }: ToastProps) => {
+  // toast will be closed after 5 seconds displayed
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  //different types of styles
+  const styles =
+    type === "SUCCESS"
+      ? "fixed top-4 right-4 z-5 p-4 rounded-md bg-green-400 text-white max-w-md"
+      : "fixed top-4 right-4 z-5 p-4 rounded-md bg-red-400 text-white max-w-md";
+
+  return (
+    <aside className={styles}>
+      <article className="flex justify-center items-center">
+        <blockquote className="text-lg font-semibold">{message}</blockquote>
+      </article>
+    </aside>
+  );
+};
+
+export default Toast;
