974b442c3c801803b310d22d3e51bbba89af4e66
Flora Li <samantha20200604@gmail.com>
2024-06-16 12:44:53 +1000

    add AppContextProvider into main.tsx

--- mern-booking-app/frontend/src/main.tsx
+++ mern-booking-app/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContext";
 
 //managing and caching asynchronous data, simplifies the process of fetching, caching, synchronizing, and updating server-side data
 const queryClient = new QueryClient({
@@ -16,7 +17,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
