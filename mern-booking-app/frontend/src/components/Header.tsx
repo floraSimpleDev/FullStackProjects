294577fd3cca5ee06a5a15fa92c5379892cb245d
Flora Li <samantha20200604@gmail.com>
2024-06-16 18:58:27 +1000

    toggle isLoggedIn

--- mern-booking-app/frontend/src/components/Header.tsx
+++ mern-booking-app/frontend/src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { FaUniversalAccess } from "react-icons/fa";
-import { RxEnter } from "react-icons/rx";
+import { RxEnter, RxExit } from "react-icons/rx";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <nav className="bg-[#493e99] py-6">
       <menu className="container mx-auto flex justify-between px-40 sm:px-20 sm:flex-wrap">
@@ -11,13 +14,23 @@ const Header = () => {
           <Link to="/">Azure Heaven</Link>
         </li>
         <li className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex items-center text-[#f09d7c] px-3 font-bold border border-[#f09d7c] rounded-xl hover:bg-[#f09d7c] hover:text-[#493e99] transition"
-          >
-            <RxEnter />
-            &nbsp; Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-hotels">My Hotels</Link>
+              <button>
+                <RxExit /> &nbsp; Sign out
+              </button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex items-center text-[#f09d7c] px-3 font-bold border border-[#f09d7c] rounded-xl hover:bg-[#f09d7c] hover:text-[#493e99] transition"
+            >
+              <RxEnter />
+              &nbsp; Sign In
+            </Link>
+          )}
         </li>
       </menu>
     </nav>
