0ab1cf0f649c37b79fc6ef14038651ba464898d8
Flora Li <samantha20200604@gmail.com>
2024-06-19 17:10:56 +1000

    set up sign in interaction in api-client.ts file

--- mern-booking-app/frontend/src/api-client.ts
+++ mern-booking-app/frontend/src/api-client.ts
@@ -1,8 +1,10 @@
 import { RegisterFormData } from "./pages/Register";
+import SignIn, { SignInFormData } from "./pages/SignIn";
 
 //import the VITE_API_BASE_URL from frontend .env file
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+//register interaction
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
     method: "POST",
@@ -20,6 +22,26 @@ export const register = async (formData: RegisterFormData) => {
   }
 };
 
+//sign in interaction
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const body = await response.json();
+
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+  return body;
+};
+
+//validate token interaction
 export const validateToken = async () => {
   const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
     credentials: "include", //tell the browser send the cookie to server, then server will send it back to browser
