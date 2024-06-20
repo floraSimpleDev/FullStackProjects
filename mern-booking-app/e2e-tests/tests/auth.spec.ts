286e897ead4ffb8572262dbfdec56bf56dc3b213
Flora Li <samantha20200604@gmail.com>
2024-06-20 19:18:23 +1000

    customize the login, register and sign out tests

--- mern-booking-app/e2e-tests/tests/auth.spec.ts
+++ mern-booking-app/e2e-tests/tests/auth.spec.ts
@@ -9,6 +9,16 @@ test("should allow the user to sign in", async ({ page }) => {
   await page.getByRole("link", { name: "Sign In" }).click();
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("123456");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
 
 test("get started link", async ({ page }) => {
