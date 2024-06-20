0c0d518f166d116910800dd5b3d7ec0224b275ca
Flora Li <samantha20200604@gmail.com>
2024-06-20 19:03:56 +1000

    test for Sign In button

--- mern-booking-app/e2e-tests/tests/auth.spec.ts
+++ mern-booking-app/e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const UI_URL = "http://localhost:5173/";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  //get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
