1724947602a288d40144d5a468acc5bc4879382d
Flora Li <samantha20200604@gmail.com>
2024-06-20 19:19:02 +1000

    compltete the whole auth test file

--- mern-booking-app/e2e-tests/tests/auth.spec.ts
+++ mern-booking-app/e2e-tests/tests/auth.spec.ts
@@ -20,15 +20,3 @@ test("should allow the user to sign in", async ({ page }) => {
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
-});
