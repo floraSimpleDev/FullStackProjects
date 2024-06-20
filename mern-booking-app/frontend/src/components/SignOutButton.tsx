2e5eba1166c2e9ca4d98cd7d9dad90af1d470ea0
Flora Li <samantha20200604@gmail.com>
2024-06-20 15:19:25 +1000

    complete interaction of SignOutButton.tsx

--- mern-booking-app/frontend/src/components/SignOutButton.tsx
+++ mern-booking-app/frontend/src/components/SignOutButton.tsx
@@ -1,8 +1,31 @@
 import { RxExit } from "react-icons/rx";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useQueryClient, useMutation } from "react-query";
 
 const SignOutButton = () => {
+  const queryClient = useQueryClient();
+  const { showToast } = useAppContext();
+
+  const mutation = useMutation(apiClient.signOut, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries("validateToken");
+      showToast({ message: "Signed Out!", type: "SUCCESS" });
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const handleClick = () => {
+    mutation.mutate();
+  };
+
   return (
-    <button className="flex items-center text-[#f09d7c] px-3 font-bold border border-[#f09d7c] rounded-xl hover:bg-[#f09d7c] hover:text-[#493e99] transition">
+    <button
+      onClick={handleClick}
+      className="flex items-center text-[#f09d7c] px-3 font-bold border border-[#f09d7c] rounded-xl hover:bg-[#f09d7c] hover:text-[#493e99] transition"
+    >
       <RxExit /> &nbsp; Sign out
     </button>
   );
