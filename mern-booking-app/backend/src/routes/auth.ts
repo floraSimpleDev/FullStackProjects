8700d51269cf05de360cbb449d7ba963795b2232
Flora Li <samantha20200604@gmail.com>
2024-06-20 15:18:50 +1000

    debug sign out response.send()

--- mern-booking-app/backend/src/routes/auth.ts
+++ mern-booking-app/backend/src/routes/auth.ts
@@ -73,6 +73,7 @@ router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
 //logout router
 router.post("/logout", (req: Request, res: Response) => {
   res.cookie("auth_token", "", { expires: new Date(0) }); //expired as while as it is created
+  res.send(); // in case of pending
 });
 
 export default router;
