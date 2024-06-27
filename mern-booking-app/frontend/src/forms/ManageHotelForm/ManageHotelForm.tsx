add347c2dd603fc30c385adfcc79febf3cc05758
Flora Li <samantha20200604@gmail.com>
2024-06-27 17:42:52 +1000

    add save button as submit button

--- mern-booking-app/frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ mern-booking-app/frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -21,15 +21,28 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
   const formMethods = useForm<HotelFormData>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = handleSubmit(() => {});
+
   return (
     <main className="border-[#493e99] border rounded-xl">
+      {/* in case to create multiple hotel forms */}
       <FormProvider {...formMethods}>
-        <form className="flex flex-col gap-10 px-40 py-10">
+        <form className="flex flex-col gap-10 px-40 py-10" onSubmit={onSubmit}>
           <DetailsSection />
           <TypeSection />
           <FacilitiesSection />
           <GuestsSection />
           <ImagesSection />
+          <section className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-[#493e99] text-white p-2 px-4 rounded-md font-bold hover:bg-[#2e2e7b]"
+            >
+              Save
+            </button>
+          </section>
         </form>
       </FormProvider>
     </main>
