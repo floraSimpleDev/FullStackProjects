39e40d314680dd1a10863f58b53e8df297c55efb
Flora Li <samantha20200604@gmail.com>
2024-06-28 21:16:06 +1000

    test add-hotel page

--- mern-booking-app/e2e-tests/tests/manage-hotels.spec.ts
+++ mern-booking-app/e2e-tests/tests/manage-hotels.spec.ts
@@ -17,3 +17,32 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByText("Sign in Successful!")).toBeVisible();
 });
+
+test("should allow user to add a hotel", async ({ page }) => {
+  //go to the add-hotel page
+  await page.goto(`${UI_URL}add-hotel`);
+
+  await page.locator('[name="name"]').fill("Test Hotel");
+  await page.locator('[name="city"]').fill("Test City");
+  await page.locator('[name="country"]').fill("Test Country");
+  await page
+    .locator('[name="description"]')
+    .fill("This is a description for the Test Hotel");
+  await page.locator('[name="pricePerNight"]').fill("100");
+  await page.selectOption('[name="starRating"]', "4");
+
+  await page.getByText("Budget").click();
+
+  await page.getByLabel("Free WiFi").check();
+  await page.getByLabel("Parking").check();
+  await page.getByLabel("Airport Shuttle").check();
+
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("2");
+
+  await page.getByRole("button", { name: "Save" }).click();
+
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+});
+
+/* test("should display hotels", async({page}) => {}) */
