b1a5ef7c05784deec2c60c1872c474fd815906d6
Flora Li <samantha20200604@gmail.com>
2024-07-04 22:44:51 +1000

    click first view details link

--- mern-booking-app/e2e-tests/tests/manage-hotels.spec.ts
+++ mern-booking-app/e2e-tests/tests/manage-hotels.spec.ts
@@ -73,7 +73,7 @@ test("should display hotels", async ({ page }) => {
 test("should edit hotel", async ({ page }) => {
   await page.goto(`${UI_URL}my-hotels`);
 
-  await page.getByRole("link", { name: "View Details" }).click();
+  await page.getByRole("link", { name: "View Details" }).first().click();
 
   await page.waitForSelector('[name="name"]', { state: "attached" });
   await expect(page.locator('[name="name"]')).toHaveValue("large hotel");
