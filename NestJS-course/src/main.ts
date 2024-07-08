002c6a6eb7e3dc278dd03756ecd1df9ce96d2f29
Flora Li <samantha20200604@gmail.com>
2024-07-08 18:50:42 +1000

    set up first Module based on controller

--- NestJS-course/src/main.ts
+++ NestJS-course/src/main.ts
@@ -1,5 +1,11 @@
 import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
+/*
+Decorator:
+@Controller()
+create a class to serve as a controller in our app 
+*/
 @Controller()
 class AppController {
   @Get()
@@ -7,3 +13,21 @@ class AppController {
     return "hi there!";
   }
 }
+
+/*
+Module:
+wrap up our controllers, one app must have one module.
+@Module()
+*/
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function boostrap() {
+  // init an instance of app
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+boostrap();
