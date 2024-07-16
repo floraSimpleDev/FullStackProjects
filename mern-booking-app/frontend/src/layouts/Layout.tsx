f09ebe9af1b886fc4278cd19b69414e909c33cb9
Flora Li <samantha20200604@gmail.com>
2024-07-16 21:02:22 +1000

    import search bar

--- mern-booking-app/frontend/src/layouts/Layout.tsx
+++ mern-booking-app/frontend/src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import SearchBar from "../components/SearchBar";
 
 interface Props {
   children: React.ReactNode;
@@ -11,6 +12,9 @@ const Layout = ({ children }: Props) => {
     <main className="flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <section className="container mx-auto px-40">
+        <SearchBar />
+      </section>
       <div className="container mx-auto py-10 px-40 flex-1">{children}</div>
       <Footer />
     </main>
