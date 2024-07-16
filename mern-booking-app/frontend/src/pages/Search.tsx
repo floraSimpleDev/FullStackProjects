ca38ad11773c813f27574009bc0697b4891c5b36
Flora Li <samantha20200604@gmail.com>
2024-07-16 21:02:39 +1000

    set up search context into search page

---
+++ mern-booking-app/frontend/src/pages/Search.tsx
@@ -0,0 +1,9 @@
+import { useSearchContext } from "../contexts/SearchContext";
+
+const Search = () => {
+  const search = useSearchContext();
+
+  return <>Search Page</>;
+};
+
+export default Search;
